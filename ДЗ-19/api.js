0277caa5b57c493a1ac6ec8eaf7bc4b4ccdf8b6c
suprunOksana <84230525+suprunOksana@users.noreply.github.com>
2021-08-29 11:38:55 +0300

    Add files via upload

---
+++ ДЗ-19/api.js
@@ -0,0 +1,53 @@
+const API_URL = 'https://reqres.in/api';
+
+
+export const checkUserCredentials = (login, password) => {
+  const requestBody = {
+    email: login,
+    password: password,
+  };
+
+  return fetch(`${API_URL}/login`, {
+    body: JSON.stringify(requestBody),
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+  }).then(e => {
+    if (e.status >= 400) {
+      throw e;
+    }
+    return e
+  }).then(e => e.json());
+};
+
+export const getUserList = (page = 1) => {
+  return fetch(`${API_URL}/users?page=${page}`)
+  .then((e) => e.json());
+};
+
+export const deleteUser = id => fetch(`${API_URL}/users/${id}`, {
+  method: 'DELETE'
+});
+
+export const createUser = (name, job) => {
+  const requestBody = {
+    name: name,
+    job: job
+  };
+
+  return fetch(`${API_URL}/users`, {
+    body: JSON.stringify(requestBody),
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    }
+  }).then(e => {
+    if (e.status >= 400) {
+      throw e;
+    }
+    return e;
+  })
+    .then(e => e.json());
+};
+
---
+++ ДЗ-19/core/Component.js
@@ -0,0 +1,69 @@
+export default class Component {
+    constructor(template, entryId) {
+        const el = document.createElement('div');
+        el.insertAdjacentHTML('afterbegin', template)
+        this._componentEl = el;
+        const virtEl = document.createElement('div');
+        virtEl.insertAdjacentHTML('afterbegin', template);
+        this._virtualTemplate = virtEl;
+        this._entryEl = document.getElementById(entryId);
+        this.state = {};
+        this.eventList = {};
+    }
+
+    getElementById(id) {
+        return this._componentEl.querySelector(`#${id}`)
+    }
+
+    querySelector(selector) {
+        return this._virtualTemplate.querySelector(selector);
+    }
+
+    getRealElementById(id) {
+        return this._componentEl.querySelector(`#${id}`);
+    }
+
+    realQuerySelector(selector) {
+        return this._componentEl.querySelector(selector);
+    }
+
+
+    addListener(selector, event, cb) {
+        this.eventList[selector] = {
+            ...this.eventList[selector],
+            [event]: [
+                ...(this.eventList[selector] ? this.eventList[selector][event] : []),
+                cb
+            ]
+        };
+    }
+
+    _initEvents() {
+        Object.keys(this.eventList).forEach(e => {
+            const el = this.realQuerySelector(e);
+            Object.keys(this.eventList[e]).forEach(event => {
+                this.eventList[e][event].forEach(cb => {
+                    el.addEventListener(event, cb);
+                });
+            });
+        });
+    }
+
+    setState(state) {
+        this.state = {
+            ...this.state,
+            ...state
+        };
+
+        this.render();
+    }
+
+    render() {
+        this._entryEl.insertAdjacentElement('afterbegin', this._componentEl);
+        this._initEvents();
+    }
+
+    dispose() {
+        this._componentEl.remove();
+    }
+}
